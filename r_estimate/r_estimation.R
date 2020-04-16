9239eb8c7413436da1130fc9cc9a425f129006b0
Ellie Abdollahi <61531556+Ellie-Abdollahi@users.noreply.github.com>
2020-04-15 22:43:23 -0400

    Add files via upload

--- r_estimate/r_estimation.R
+++ r_estimate/r_estimation.R
@@ -1,31 +1,23 @@
-#Maximum Likelihood to estimate r
+#Maximum Likelihood to estimate r (Hiroshi Nishiura et al. 2009)
 
 rm(list = ls())
-library(Stats)
+library(stats)
 library(utils)
+
+
+path<-file.path("~","Documents", "CFR_Data_Canada.csv")
+my_data=read.table(path, header = TRUE, sep = ",", stringsAsFactors = FALSE)
 
 # path<-file.path("~","Documents", "CFR_Data_US.csv")
 # my_data=read.table(path, header = TRUE, sep = ",", stringsAsFactors = FALSE)
 
-path<-file.path("~","Documents", "CFR_Data_NY.csv")
-my_data=read.table(path, header = TRUE, sep = ",", stringsAsFactors = FALSE)
-
-#Calculating cumulatives for NYC
-#We use this part when number of incidence is available
-
-# my_data$cum.case[1]=my_data$case[1]
-# my_data$cum.death[1]=my_data$death[1]
-# 
-# 
-# for (i in 2:length(my_data$Date)){
-#   my_data$cum.case[i] = my_data$cum.case[i-1]+my_data$case[i]
-#   my_data$cum.death[i] = my_data$cum.death[i-1]+my_data$death[i]
-#  }
+# path<-file.path("~","Documents", "CFR_Data_NY.csv")
+# my_data=read.table(path, header = TRUE, sep = ",", stringsAsFactors = FALSE)
 
 
 
-t = length(my_data$cum.case)
-rmax = array()
+t = length(my_data$Date)
+
 C= array()
 C2 = array()
 
@@ -38,18 +30,19 @@ for (i in 1:t){
 }
 
 
-rmax = array()
+rmax = array() #Some of them should be set as zero manually, the ML function throw error for them
 
 #For each data point, substitute "t" with that day
+
 ll <- function(r){
   
     ((-C2[t])*(1-exp(-r))+ (C[t]-C[1])*exp(-r))
 }
 
-rmax[t] <- uniroot(ll, lower = 0 , upper = 1)[[1]]
+rmax[t] <- uniroot(ll, lower = 0 , upper = 1)[[1]] 
 
-rmax[1]=0
+#We will use this vector in the main function, when estimating CFR at each time
 
-# write.table(my_data, file = "CFR_Data_NY.csv", sep=",")
-# write.table(rmax, file = "r_estimate_US.csv", sep = ",")
-# write.table(rmax, file = "r_estimate_NYC.csv", sep = ",")
+write.table(rmax, file = "r_estimate_Canada.csv", sep = ",")
+#write.table(rmax, file = "r_estimate_US.csv", sep = ",")
+#write.table(rmax, file = "r_estimate_NYC.csv", sep = ",")
